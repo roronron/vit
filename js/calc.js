6d1c42fddbed3cb497fde925d22b622baa317a97
Ronald Brown <rbrown780@gmail.com>
2021-08-31 15:24:52 +0700

    Add files via upload

---
+++ js/calc.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+function retin() {
+alert (1111)
+document.getElementById("iu").value;
+var num =document.getElementById("iu").value;
+var val = num * .0000003 * 1000000;
+var valmg = num * .0003;
+document.getElementById("mcg").value = val;
+document.getElementById("mg").value = valmg;
+}
+
+function beta() {
+	alert (2222)
+document.getElementById("iu").value;
+var num =document.getElementById("iu1").value;
+var val = num * .0000006 * 1000000;
+var valmg = num * .0006;
+document.getElementById("mcg1").value = val;
+document.getElementById("mg1").value = valmg;
+}
+
+function d3() {
+	alert (3333)
+document.getElementById("iud3").value;
+var num =document.getElementById("iud3").value;
+var val = num * .0000006 * 1000000;
+var valmg = num * .0006;
+document.getElementById("mcgd3").value = val;
+document.getElementById("mgd3").value = valmg;
+}
